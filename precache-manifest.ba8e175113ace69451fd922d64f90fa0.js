e2e3afb362400bb773fa0330257c388b27a1afd5
Ash031 <a77284@alunos.uminho.pt>
2020-10-14 21:18:01 +0100

    Updates

---
+++ precache-manifest.ba8e175113ace69451fd922d64f90fa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed53c5d72c22bba7a2641ab8179e8265",
+    "url": "/VillageSim/index.html"
+  },
+  {
+    "revision": "9f81eb63e4b4ab384e88",
+    "url": "/VillageSim/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a0cdc20ae0b20d380de2",
+    "url": "/VillageSim/static/js/2.14be2578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VillageSim/static/js/2.14be2578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f81eb63e4b4ab384e88",
+    "url": "/VillageSim/static/js/main.056e9fd8.chunk.js"
+  },
+  {
+    "revision": "d6dc045408f0ab09de69",
+    "url": "/VillageSim/static/js/runtime-main.93fee330.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VillageSim/static/media/logo.5d5d9eef.svg"
+  }
+]);
